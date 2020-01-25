bcfcfefa08f4e74cca2520ed0ca27850fd3ea6a8
Xiaoyu-freshman <59091572+Xiaoyu-freshman@users.noreply.github.com>
2020-01-25 15:03:28 +0100

    Update task2.py

--- assignment1/task2.py
+++ assignment1/task2.py
@@ -1,3 +1,6 @@
+import datetime
+starttime = datetime.datetime.now()
+
 import numpy as np
 import utils
 import matplotlib.pyplot as plt
@@ -16,11 +19,16 @@ def calculate_accuracy(X: np.ndarray, targets: np.ndarray, model: BinaryModel) -
     """
     # Task 2c
     output=model.forward(X)
-    count=0
-    for i in range(len(output)):
-        if abs(output[i]-targets[i])<=0.5:
-            count+=1
-    accuracy = count/len(output)
+    mark=abs(output-targets)<=0.5
+    count=np.zeros(output.shape)
+    count[mark]=1
+    accuracy = np.sum(count)/len(output)
+
+#     count=0
+#     for i in range(len(output)):
+#         if abs(output[i]-targets[i])<=0.5:
+#             count+=1
+#     accuracy = count/len(output)
     return accuracy
 
 
@@ -39,15 +47,13 @@ def train(
     #Xiaoyu
     X_train=pre_process_images(X_train)
     X_val=pre_process_images(X_val)
-    print('X_val',X_val.shape)
+    #print('X_val',X_val.shape)
     X_test=pre_process_images(X_test)
     #Xiaoyu
     
     # Utility variables
     num_batches_per_epoch = X_train.shape[0] // batch_size
-    print('num_batches_per_epoch',num_batches_per_epoch)
     num_steps_per_val = num_batches_per_epoch // 5
-    print('num_steps_per_val',num_steps_per_val)
     train_loss = {}
     val_loss = {}
     train_accuracy = {}
@@ -75,29 +81,22 @@ def train(
             # Track training loss continuously
             _train_loss = cross_entropy_loss(Y_batch,output)
             train_loss[global_step] = _train_loss
-            #print('training loss:',train_loss[global_step])
             
             # Track validation loss / accuracy every time we progress 20% through the dataset
             if global_step % num_steps_per_val == 0:
                 _val_loss = cross_entropy_loss(Y_val,model.forward(X_val))
                 val_loss[global_step] = _val_loss
-
                 train_accuracy[global_step] = calculate_accuracy(
                     X_train, Y_train, model)
                 val_accuracy[global_step] = calculate_accuracy(
-                    X_val, Y_val, model)
-                #print('val_loss',val_loss[global_step])
-            #print('global_step',global_step)
-            
+                    X_val, Y_val, model)            
             global_step += 1
     return model, train_loss, val_loss, train_accuracy, val_accuracy
-
 # Load dataset
 category1, category2 = 2, 3
 validation_percentage = 0.1
 X_train, Y_train, X_val, Y_val, X_test, Y_test = utils.load_binary_dataset(
     category1, category2, validation_percentage)
-
 # hyperparameters
 num_epochs = 50
 learning_rate = 0.2
@@ -108,33 +107,40 @@ model, train_loss, val_loss, train_accuracy, val_accuracy = train(
     learning_rate=learning_rate,
     batch_size=batch_size,
     l2_reg_lambda=l2_reg_lambda)
-
+# print(model.w.shape)
+# print(model.forward(X_train).shape)
 print("Final Train Cross Entropy Loss:",
       cross_entropy_loss(Y_train, model.forward(X_train)))
 print("Final Validation Cross Entropy Loss:",
       cross_entropy_loss(Y_test, model.forward(X_test)))
 print("Final Test Cross Entropy Loss:",
       cross_entropy_loss(Y_val, model.forward(X_val)))
-
-
+#print(val_loss)
 print("Train accuracy:", calculate_accuracy(X_train, Y_train, model))
 print("Validation accuracy:", calculate_accuracy(X_val, Y_val, model))
 print("Test accuracy:", calculate_accuracy(X_test, Y_test, model))
 
 
 # Plot loss
-#plt.ylim([0., .4]) 
+plt.ylim([0., .4]) 
 utils.plot_loss(train_loss, "Training Loss")
 utils.plot_loss(val_loss, "Validation Loss")
+plt.xlabel('Gradient Step')
+plt.ylabel('Loss')
 plt.legend()
-plt.savefig("binary_train_loss.png")
+plt.savefig("binary_train_loss_ep500.png")
 plt.show()
 
 
 # Plot accuracy
-#plt.ylim([0.93, .99])
+plt.ylim([0.93, .99])
 utils.plot_loss(train_accuracy, "Training Accuracy")
 utils.plot_loss(val_accuracy, "Validation Accuracy")
+plt.xlabel('Gradient Step')
+plt.ylabel('Loss')
 plt.legend()
-plt.savefig("binary_train_accuracy.png")
+plt.savefig("binary_train_accuracy_ep500.png")
 plt.show()
+
+endtime = datetime.datetime.now()
+print (endtime - starttime)
